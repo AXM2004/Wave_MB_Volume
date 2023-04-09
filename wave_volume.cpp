df1cf11588dd0fcbad25649bd6185e2d92b87214
AXM2004 <115492379+AXM2004@users.noreply.github.com>
2023-04-09 17:05:21 +0200

    Add files via upload

---
+++ wave_volume.cpp
@@ -0,0 +1,27 @@
+﻿/** Wave volume counting application
+* Alpha version
+* coded by AXM
+* Licence CC0
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int samplerate = 0;
+	int lenght = 0;
+	int bits = 0;
+	int channels=1;
+		cout << "Enter bits number in file" << endl;
+	cin >> bits;
+	cout << "Enter samplerate" << endl;
+	cin >> samplerate;
+	cout << "Enter file lenght in seconds" << endl;
+	cin >> lenght;
+	cout << "Enter number of channels" << endl;
+	cin >> channels;
+	int volume= samplerate * bits * lenght * channels / 8 / 1024 / 1024;
+	cout << "approximate volume in MBs: " << volume << "MB" << endl;
+	return 0;
+}
+
